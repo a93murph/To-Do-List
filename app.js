4ae7c9929fff483a6e939cc8b34fe0c35d22f18f
Allison Murphy <amurph@Allisons-MBP.attlocal.net>
2021-05-03 07:54:05 -0400

    edit list format

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const input = document.querySelector("input");
-const output = document.querySelector(".output");
-const updateOutput = (x) => {
-  console.log("x", x);
-  output.innerText += x;
-};
+// const output = document.querySelector(".output");
 
-console.log(input);
+const updateOutput = (x) => {
+  const ul = document.querySelector(".list-none");
+  const li = document.createElement("li");
+  ul.appendChild(li);
+  // li.innerHTML = <input type="button" />;
+  li.innerText += x;
+};
 
 input.addEventListener("keyup", function (event) {
   let listVal = input.value;
   if (event.keyCode === 13) {
     updateOutput(listVal);
+    // li.innerHTML = (
+    //   <input
+    //     type="button"
+    //     class="m-8 p-4 px-10 w-1/2 text-3xl font-thin italic shadow-xl outline-none"
+    //   />
+    // );
     input.value = "";
   }
 });

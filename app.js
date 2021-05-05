66cd5455870b9e6590f8bac025bff12a3565753c
Allison Murphy <amurph@Allisons-MBP.attlocal.net>
2021-05-05 08:01:44 -0400

    add style sheet

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
 const input = document.querySelector("input");
-// const output = document.querySelector(".output");
+const ul = document.querySelector(".list-none");
 
 const updateOutput = (x) => {
-  const ul = document.querySelector(".list-none");
   const li = document.createElement("li");
   ul.appendChild(li);
-  // li.innerHTML = <input type="button" />;
   li.innerText += x;
+  li.className = "toggle";
 };
 
 input.addEventListener("keyup", function (event) {
   let listVal = input.value;
   if (event.keyCode === 13) {
     updateOutput(listVal);
-    // li.innerHTML = (
-    //   <input
-    //     type="button"
-    //     class="m-8 p-4 px-10 w-1/2 text-3xl font-thin italic shadow-xl outline-none"
-    //   />
-    // );
     input.value = "";
   }
 });
